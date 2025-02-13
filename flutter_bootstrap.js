97acc2c57701c1eea496ae269ed7211b5c6f6022
hsik421 <hsik421@users.noreply.github.com>
2025-02-13 09:06:27 +0000

    deploy: a9dc2121756657db065e5df55becdb992337a6e4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3157552110"
+    serviceWorkerVersion: "691868919"
   }
 });
